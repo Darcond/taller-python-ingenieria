d6a133df062727f5982d9e3f1aa546fcd94b83d8
Jersy <jersyacevedo10@gmail.com>
2025-10-25 19:10:31 -0500

    ejercicio 2(experimentos)

--- Modulo 3/experimentos.py
+++ Modulo 3/experimentos.py
@@ -1,12 +1,12 @@
 equipo_A = {("EXP-001", "2023-10-01"), ("EXP-002", "2023-10-02"), ("EXP-003", "2023-10-03")}
 equipo_B = {("EXP-002", "2023-10-02"), ("EXP-004", "2023-10-04"), ("EXP-001", "2023-10-01")}
 
-# Operaciones con conjuntos
+# Operaciones 
 exitos_comunes = equipo_A & equipo_B
 exitos_unicos = equipo_A | equipo_B
 solo_equipo_A = equipo_A - equipo_B
 
-# Impresión de resultados
+#  resultados
 print(f"\nExperimentos exitosos comunes: {exitos_comunes}")
 print(f"Total de experimentos exitosos únicos: {len(exitos_unicos)}")
 print(f"Experimentos solo realizados por equipo A: {solo_equipo_A}")
